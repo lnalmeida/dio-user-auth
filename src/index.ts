b9b143180cd4f3dee07f21c5283a8386d662e94c
lnalmeida <l.n.almeida.ti@gmail.com>
2021-11-01 00:32:29 -0300

    Added Typescript && Basic configurations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Executei');
+import express, { NextFunction, Request, Response } from "express";
+
+import HttpStatusCode from "./consts/HttpStatusCode";
+
+
+const app: any = express();
+const port: number = process.env.PORT | 3126;
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(HttpStatusCode.OK).send({ message: "Application Online!!!" });
+});
+
+app.listen(port, () => {
+  console.log(" App listening on port 3126");
+});
+
+export default app;
