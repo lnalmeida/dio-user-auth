f4f3cd0afa250ce422d7aacffed7a0f699e390f6
lnalmeida <l.n.almeida.ti@gmail.com>
2021-11-05 19:34:57 -0300

    Added error threatment, crud methods ok

--- src/controllers/users/user.controller.ts
+++ src/controllers/users/user.controller.ts
@@ -22,95 +22,45 @@ export class UserController {
 
   public testRouteUsers = (req: Request, res: Response, next: NextFunction) =>
   {
-    try {
       res.status(this.httpStatusCode.OK).send('Route "/users" working OK!');  
-    } catch (error) {
-        const app_error = new Error(`Error testing Routes: ${error}`);
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);
-    }
   }
-
-  private getUserByEmail = async (email:string) => {
-    try {
-      const userExists = await UserRepository.getUserByEmail(email);
-      return userExists ? true : false;
-    } catch (error) {
-      throw new Error(`Error getting user by email: ${error}`);
-    }
-  }
-
-  // private getUserByUsername = async (username: string) => {
-  //   try {
-  //     const userExists = await UserRepository.getUserByUsername(username);
-  //     return userExists ? true : false;
-  //   } catch (error) {
-  //     throw new Error(`Error getting user by username: ${error}`);
-  //   }
-  // }
-
 
   public getAllUsers = async (req: Request, res: Response, next: NextFunction) =>
   {
-    try {
       const users = await UserRepository.getAllUsers();
       res.status(this.httpStatusCode.OK).send(users);  
-    } catch (error) {
-        const app_error =new Error(`Error getting the list of users: ${error}`);
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);
-    }
   }
 
   public getUserById = async (req: Request, res: Response, next: NextFunction) => 
   {
-    try {
       const userId = req.params.uuid;
-      console.log(`UserController - getUserById - ${userId}`)
       const user = await UserRepository.getUserById(userId);
       if (!user) {
         res.status(this.httpStatusCode.NOT_FOUND).send(`User with id ${userId} not found`);
       } else {
       res.status(this.httpStatusCode.OK).send(user);   
       }
-    } catch (error) {
-        const app_error = new Error(`Error while getting user with id ${req.params.id}`);
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);
-    }
   }
 
   public createUser = async (req: Request, res: Response, next: NextFunction) => 
   {
-    try {
       const user = req.body;
       await UserRepository.createUser(user);
       res.status(this.httpStatusCode.CREATED).send('User created with success!!');
-    } catch (error) {
-        const app_error =new Error(`Error creating user: ${error}`);
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);      
-    }
   }
 
-  public  updateUser = async (req: Request, res: Response, next: NextFunction) => 
+  public updateUser = async (req: Request, res: Response, next: NextFunction) => 
   {
-    try {
-      const user = req.body;
-      await UserRepository.updateUser(user);
-      console.log('foi pra repositorio')
+      const updatedUser = req.body
+      updatedUser.uuid = req.params.uuid;
+      await UserRepository.updateUser(updatedUser);
       return res.status(this.httpStatusCode.OK).send('User updated with success!!');
-    } catch (error) {
-        const app_error = new Error(`Error updating user id: ${req.params.id}`)
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);      
-    }
   }
 
   public deleteUser = async (req: Request, res: Response, next: NextFunction) => 
   {
-    try {
       const uuid = req.params.uuid;
       await UserRepository.deleteUser(uuid);
       res.status(this.httpStatusCode.OK).send(`User removed with success!!!!`);
-    } catch (error) {
-        const app_error = new Error(`Error deleting user id: ${req.params.id}`);
-        res.status(this.httpStatusCode.INTERNAL_SERVER_ERROR).send(app_error);      
-    }
   }
 }
--- src/database/repositories/user.repository.ts
+++ src/database/repositories/user.repository.ts
@@ -1,3 +1,4 @@
+import  DatabaseError from '../../errors/databaseError';
 import User from '../../models/user.model';
 import db  from '../db';
 
@@ -8,60 +9,97 @@ class UserRepository {
     }
     
     async getAllUsers(): Promise<User[]> {
-        const queryStr = `
-            SELECT uuid, name, username, email, avatar, created_at 
-            FROM application_user;`
-        const {rows} = await this.db.query(queryStr);
-        const users = rows;
-        return users || [];            
+        try {
+            const queryStr = `
+                SELECT uuid, name, username, email, avatar, created_at 
+                FROM application_user;`
+            const {rows} = await this.db.query(queryStr);
+            const users = rows;
+            return users || [];            
+        } catch (error) {
+            if (error instanceof DatabaseError) {
+                throw new DatabaseError('Erro ao carregar registros', error);
+            } else {
+                throw error;
+            }
+        }
     }
     
     async getUserById(uuid: string): Promise<User> {
-        console.log('UserRepository - getUserById')
-        const queryStr = `
-            SELECT uuid, name, username, email, avatar, created_at 
-            FROM application_user 
-            WHERE uuid = $1;`
-            const values = [uuid];
-            const {rows} = await this.db.query<User>(queryStr, values);
-        const [userById] = rows;
-        return userById || null;
+        try {
+            
+            const queryStr = `
+                SELECT uuid, name, username, email, avatar, created_at 
+                FROM application_user 
+                WHERE uuid = $1;`
+                const values = [uuid];
+                const {rows} = await this.db.query<User>(queryStr, values);
+            const [userById] = rows;
+            return userById || null;
+        } catch (error) {
+            if (error instanceof DatabaseError) {
+                throw new DatabaseError('Erro ao carregar registro', error);
+            } else {
+                throw error;
+            }
+        }
     }
     
     async createUser(user: User): Promise<string> {
-        const {name, username, password, email, avatar} = user 
-        const insert_script = `
-                INSERT INTO application_user (name, username, password, email, avatar)
-                VALUES ($1, $2, crypt($3, 'salt'), $4, $5)
-                RETURNING uuid as varchar;`
-        const values = [name, username, password, email, avatar];
-
-        const {rows} = await this.db.query<{uuid: string}>(insert_script, values);
-        const [newUser] = rows;
-        return newUser.uuid;   
+        try {
+            
+            const {name, username, password, email, avatar} = user 
+            const insert_script = `
+                    INSERT INTO application_user (name, username, password, email, avatar)
+                    VALUES ($1, $2, crypt($3, 'salt'), $4, $5)
+                    RETURNING uuid as varchar;`
+            const values = [name, username, password, email, avatar];
+    
+            const {rows} = await this.db.query<{uuid: string}>(insert_script, values);
+            const [newUser] = rows;
+            return newUser.uuid;   
+        } catch (error) {
+            if (error instanceof DatabaseError) {
+                throw new DatabaseError('Erro ao criar registro', error);
+            } else {
+                throw error;
+            }
+        }
     }
 
     async updateUser(user: User): Promise<void> {
-        const update_script = `
-        UPDATE application_user 
-        SET 
-        name = $1, 
-        username = $2, 
-        password = crypt($3, 'salt'), 
-        email = $4, 
-        avatar = $5
-        WHERE uuid = $6;`
-        const values = [user.name, user.username, user.password, user.email, user.avatar, user.uuid];
-        await this.db.query(update_script, values);
-        console.log('executou a query')
+       try {
+           const {uuid, name, username, password, email, avatar} = user;
+           const update_script = `
+           UPDATE application_user 
+            SET name = $1, username = $2, password = crypt($3, 'salt'), 
+                email = $4, avatar = $5
+            WHERE uuid = $6`;
+           const values = [name, username, password, email, avatar, uuid];
+           await this.db.query(update_script, values);
+       } catch (error) {
+           if (error instanceof DatabaseError) {
+               throw new DatabaseError('Erro ao atualizar registro', error);
+           } else {
+               throw error;
+           }
+       }
     }
     
     async deleteUser(uuid: string): Promise<void> {
-        const delete_script = `
-            DELETE FROM application_user 
-            WHERE uuid = $1;`
-        const values = [uuid];
-        await this.db.query(delete_script, values);
+        try {
+            const delete_script = `
+                DELETE FROM application_user 
+                WHERE uuid = $1;`
+            const values = [uuid];
+            await this.db.query(delete_script, values);
+        } catch (error) {
+            if (error instanceof DatabaseError) {
+                throw new DatabaseError('Erro ao deletar registro', error);
+            } else {
+                throw error;
+            }
+        }
     }
 }
 
